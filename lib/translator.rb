fa0c941fbcb92b76b091eb9dd89d601d7951298c
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-15 13:34:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  library = load_library("./lib/emoticons.yml")
+  library = load_library(file_path)
   
   emoticon = library.keys.find do |key|
     library[key][:english] == emoticon
@@ -24,7 +24,7 @@ def get_japanese_emoticon(file_path, emoticon)
 end
 
 def get_english_meaning(file_path, emoticon)
-  library = load_library("./lib/emoticons.yml")
+  library = load_library(file_path)
   emoticon = library.keys.find do |key|
     library[key][:japanese] == emoticon
   end
