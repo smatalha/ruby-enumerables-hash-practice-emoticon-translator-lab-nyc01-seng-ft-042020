cbdc7175836649741b78f1e7a8bcb7b5b60b6429
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-15 14:20:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,11 +16,10 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
-  
   emoticon = library.keys.find do |key|
     library[key][:english] == emoticon
   end
-  emoticon ? emoticon : "Sorry, that emoticon was not found"
+  emoticon ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoticon)
